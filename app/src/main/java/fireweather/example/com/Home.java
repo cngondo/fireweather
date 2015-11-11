cb9a6f730332f646733114b8c2b895fe7596e77e
cngondo <devngondo@gmail.com>
2015-11-11 11:45:55 +0300

    Added firebase elements and UI

---
+++ app/src/main/java/fireweather/example/com/Home.java
@@ -0,0 +1,52 @@
+package fireweather.example.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+//        Initialize firebase
+        Firebase.setAndroidContext(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Linked to the UI
+        Button rainy = (Button)findViewById(R.id.btnRainy);
+        Button sunny = (Button)findViewById(R.id.btnSunny);
+        TextView condition = (TextView)findViewById(R.id.tvCondition);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
